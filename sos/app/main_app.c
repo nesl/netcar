19bfcca03e3da7f8cdc68c8d26b16cb192ad473a
schmid <schmid@01f79039-b926-0410-8c71-b0234c05aee7>
2007-01-17 18:33:22 +0000

    just some cosmetic changes in the code

--- sos/app/main_app.c
+++ sos/app/main_app.c
@@ -5,7 +5,7 @@ mod_header_ptr accel_sensor_get_header();
 mod_header_ptr accel_test_app_get_header();
 
 void sos_start(void) {
-	ker_register_module(loader_get_header());
-	ker_register_module(accel_sensor_get_header());
-	ker_register_module(accel_test_app_get_header());
+  ker_register_module(loader_get_header());
+  ker_register_module(accel_sensor_get_header());
+  ker_register_module(accel_test_app_get_header());
 }
