6bb09f786cf66d6d2fbc2bf8940e66f1832d687c
schmid <schmid@01f79039-b926-0410-8c71-b0234c05aee7>
2007-01-20 00:01:04 +0000

    removed all the visual code.

--- sos/BaseStation.py
+++ sos/BaseStation.py
@@ -5,8 +5,7 @@ import socket
 import sys
 import struct
 import time
-import wx
-import wx.lib.plot as plot
+
 
 ACCELEROMETER_MODULE = 0x80
 
@@ -14,20 +13,6 @@ ACCELEROMETER_DATA = 41
 
 SAMPLES_PER_MSG = 20
 SAMPLE_RATE = 50
-
-EVT_RESULT_ID = wx.NewId()
-
-def EVT_RESULT(win, func):
-    """Define Result Event."""
-    win.Connect(-1, -1, EVT_RESULT_ID, func)
-
-class ResultEvent(wx.PyEvent):
-    """Simple event to carry arbitrary result data."""
-    def __init__(self, data):
-        """Init Result Event."""
-        wx.PyEvent.__init__(self)
-        self.SetEventType(EVT_RESULT_ID)
-        self.data = data
 
 class SocketClient:
     """ """
@@ -54,18 +39,8 @@ class SocketClient:
             print "Error: No connection to command server..."
 
 
-class BaseStation(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(180, 280))
-
-        self.client = plot.PlotCanvas(self)
-        self.Show(True)
-
-        self.index = 0
-        self.d0 = []
-        self.d1 = []
-        
-        EVT_RESULT(self,self.OnResult)
+class BaseStation:
+    def __init__(self):
 
         self.sc = SocketClient("127.0.0.1", 7915)
         try:
@@ -73,26 +48,7 @@ class BaseStation(wx.Frame):
         except thread.error:
             print error
 
-        try:
-            thread.start_new_thread(self.output_thread, ())
-        except thread.error:
-            print error
-        
-
-    def OnResult(self, event):
-
-        accel0 = event.data['accel0']
-        accel1 = event.data['accel1']
-
-        self.d0 += accel0
-        self.d1 += accel1
-        
-        line0 = plot.PolyLine(self.d0, legend='accel0', colour='green', width=1)
-        line1 = plot.PolyLine(self.d1, legend='accel1', colour='red', width=1)
-        gc = plot.PlotGraphics([line0, line1], 'Line Graph', 'X Axis', 'Y Axis')
-        # the X axis shows 60 seconds from the last sample.
-        self.client.Draw(gc, xAxis= (self.d0[max(-500, -len(self.d0))][0], self.d0[-1][0]), yAxis= (0,1024))
-        
+        self.output_thread()
             
         
     def input_thread(self):
@@ -152,26 +108,15 @@ class BaseStation(wx.Frame):
 
                         nodes[src_addr]['seq_nr'] = seq_nr
                         nodes[src_addr]['last_seen'] = time_rx
-                        wx.PostEvent(self, ResultEvent({'accel0': d0, 'accel1': d1}))
-
-                    #print seq_nr
-                    #print accel0
-                    #print accel1
+                        
+                        print d0
+                        print d1
                 lastdata = -1
             else:
                 lastdata = data
 
         
-class MyApp(wx.App):
-    def OnInit(self):
-        frame = BaseStation(None, -1, 'Plotting')
-        frame.Show(True)
-        self.SetTopWindow(frame)
-        
-        return True
-    
 if(__name__ == "__main__"):
 
-    app = MyApp(0)
-    app.MainLoop()
+    BaseStation()
 
