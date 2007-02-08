b288f1bd714a24ce2b630a10f59026596fb23634
schmid <schmid@01f79039-b926-0410-8c71-b0234c05aee7>
2007-02-08 06:07:28 +0000

    added gps logging. Note that it is hardlinked to ttyUSB3

--- sos/BaseStation_visual.py
+++ sos/BaseStation_visual.py
@@ -8,7 +8,8 @@ import time
 import os.path
 import wx
 import wx.lib.plot as plot
-from rpy import *
+import gps
+#from rpy import *
 
 ACCELEROMETER_MODULE = 0x80
 
@@ -145,12 +146,17 @@ class BaseStation(wx.Frame):
 
         self.Show(True)
 
+        self.g = gps.GpsThread("/dev/ttyUSB3")
+
         self.index = 0
         self.time = 0
         self.d0 = {}
         self.d1 = {}
         self.d2 = {}
         self.file = 0
+        self.gpsfile = 0
+        self.dir = str(int(time.time())) + "/"
+        os.mkdir(self.dir)
 
         EVT_RESULT(self,self.OnResult)
 
@@ -158,11 +164,13 @@ class BaseStation(wx.Frame):
         """Handles the button events and tags the datastream with the action.
         """
         if not self.file:
-            self.file = open("event.log", 'w')
+            self.file = open(self.dir + "event.log", 'w')
+            self.file.write("#time\tevent\n\n")
+            self.file.flush()
         tag = e.GetEventObject().GetLabel()
         
         self.file.write("%s\t%s\n"%(self.time, tag))
-
+        self.file.flush()
 
     # Handlers for events on "Connect" "Plot" and "Exit"
     def OnConnect(self, e):
@@ -286,8 +294,8 @@ class BaseStation(wx.Frame):
                     #try to find out the sample times
                     if src_addr not in nodes.keys():
                         #never seen the node before.
-                        nodes[src_addr] = {'last_seen': time_rx, 'file': open(str(src_addr)+".log", 'w')}
-
+                        nodes[src_addr] = {'last_seen': time_rx, 'file': open(self.dir + str(src_addr)+".log", 'w')}
+                        nodes[src_addr]['file'].write("#time\taccel0\taccel1\taccel2\n\n")
                     else:
                         d0 = []
                         d1 = []
@@ -303,7 +311,20 @@ class BaseStation(wx.Frame):
                             nodes[src_addr]['file'].write('%f\t%d\t%d\t%d\n'%(time_rx - (SAMPLES_PER_MSG-i)*1/float(SAMPLE_RATE), accel0[i], accel1[i], accel2[i]))
 
                         nodes[src_addr]['last_seen'] = time_rx
-                        wx.PostEvent(self, ResultEvent({'src_addr': src_addr, 'accel0': d0, 'accel1': d1, 'accel2': d2}))
+                        if src_addr == 0:
+                            if not self.gpsfile:
+                                self.gpsfile = open(self.dir + 'gps.log', 'w')
+                                self.gpsfile.write("#nodeTime\tlat\tlon\talt\tUTC\tspeed\tsatellites\tpdop\tsatelliteStats\n\n")
+                            self.gpsfile.write("%f\t"%(self.time,))
+                            self.gpsfile.write("%f\t%f\t%f\t"%self.g.getCoordinates())
+                            self.gpsfile.write("%s\t%s\t"%(self.g.getTime(), self.g.getSpeed()))
+                            self.gpsfile.write("%d\t"%self.g.getSatellites())
+                            self.gpsfile.write("%f\t"%(self.g.getPDOP(),))
+                            s = self.g.getSatelliteStatistics()
+                            self.gpsfile.write((12*"%d\t")%tuple(s[0]))
+                            self.gpsfile.write((12*"%d\t")%tuple(s[1]))
+                            self.gpsfile.write("\n")
+                            wx.PostEvent(self, ResultEvent({'src_addr': src_addr, 'accel0': d0, 'accel1': d1, 'accel2': d2}))
 
                     #print accel0
                     #print accel1
