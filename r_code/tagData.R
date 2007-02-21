b2252009e19d4e40265ddbb6778289230f8b4e12
akhi3030 <akhi3030@01f79039-b926-0410-8c71-b0234c05aee7>
2007-02-21 21:22:01 +0000

    Bugfix: tagData.R

--- r_code/tagData.R
+++ r_code/tagData.R
@@ -4,12 +4,12 @@ tag = function(data, events) {
   
   for(i in 1:nrow(events)){
     # break if ran out of data
-    if (events[i,1] > data(length(data$time), 1))
-      break
+    #if (events[i,1] > data[nrow(data),1])
+    #  break
     # Calling Mark's function
     cc = numatch(data[,1],events[i,1])
     data$event[cc] = events[i,2]
-   }
+  }
 
   # Plotting the x acceleration against y and making it color coded
   plot(data$accel0, data$accel1, col = data$event)
