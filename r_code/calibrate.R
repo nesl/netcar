a012ea3338202782e59706934a90d530902a0c20
akhi3030 <akhi3030@01f79039-b926-0410-8c71-b0234c05aee7>
2007-02-27 20:41:33 +0000

    fixed r_code/calibrate.R so that it removes corrupted data and time packets

--- r_code/calibrate.R
+++ r_code/calibrate.R
@@ -4,27 +4,57 @@ calibrate = function(fileName, dataSize) {
 
   # Denote the initial calibration time
   START = 100
-  # Used for corruption of packets
-  MAX_TIME = 10000
+  # Used for corruption of data packets
   MAX_ACCEL = 1024
+  # Used for corruption of time stamp
+  PERIOD = 1/50
+  THRESHOLD = 50
   # To convert from adc readings to gs
   H34C_CALIB = 0.00880646
 
-  # Read the file
-  l1 = read.table(fileName, head = T, nrows = dataSize)
+  # Read the data file
+  data = read.table(fileName, head = T, nrows = dataSize)
+
+  # remove bogus data based on corruption of acceleration data
+  data = data[data$accel0 <= MAX_ACCEL & data$accel1 <= MAX_ACCEL & data$accel2 <= MAX_ACCEL,]
+
+  data_set = 0
+  length(data_set) = length(data$time)
+  data_set[1:START] = TRUE
+
+  # remove bogus data based on corruption of time stamp
+  for(i in START:(length(data$time) - 1)) {
+    # assume that the data point at START is a valid data point
+    # remove the next data point if it is smaller than this data point
+    if(data$time[i] > data$time[i + 1]) {
+      print("deleted data less than time")
+      data_set[i + 1] = FALSE
+    }
+    # remove the next data point if it is bigger than the allowed THRESHOLD
+    # The THRESHOLD is used to account for packet lossese 
+    else if((data$time[i] - data$time[i + 1]) > THRESHOLD) {
+      data_set[i + 1] = FALSE
+      print("deleted data more than time")
+    }
+    else {
+      data_set[i + 1] = TRUE
+    }
+  }
+
+  print("done")
   
-  # remove bogus data based on packet corruption
-  l1 = l1[l1$accel0 <= MAX_ACCEL & l1$accel1 <= MAX_ACCEL & l1$accel2 <= MAX_ACCEL,]
-  l1 = l1[l1$time < MAX_TIME & l1$accel0 > 0 & l1$accel1 > 0 & l1$accel2 > 0,]
+  data = data[data_set == TRUE,]
+
+  data
   
   # calculate the mean of the first samples for calibration
-  l1calib = mean(l1[1:START,])
+#  datacalib = mean(data[1:START,])
   
   # calibrate the data
-  l1$accel0 = H34C_CALIB * (l1$accel0 - l1calib["accel0"])
-  l1$accel1 = H34C_CALIB * (l1$accel1 - l1calib["accel1"])
-  l1$accel2 = H34C_CALIB * (l1$accel2 - l1calib["accel2"])
+#  data$accel0 = H34C_CALIB * (data$accel0 - datacalib["accel0"])
+#  data$accel1 = H34C_CALIB * (data$accel1 - datacalib["accel1"])
+#  data$accel2 = H34C_CALIB * (data$accel2 - datacalib["accel2"])
 
   # Return the data set
-  l1
+#  data
 }
